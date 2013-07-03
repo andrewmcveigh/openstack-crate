45d7491eb8f88fa0d44a734ecb09096878e5f07f
Andrew Mcveigh <me@andrewmcveigh.com>
2013-07-03 14:30:23 +0200

    Cinder ran through

--- src/pallet/crate/openstack/cinder.clj
+++ src/pallet/crate/openstack/cinder.clj
@@ -15,14 +15,18 @@
   (restart-services :flag "restart-iscsi" "iscsitarget" "open-iscsi")
   (mysql/create-user user password "root" mysql-root-pass)
   (mysql/create-database "cinder" "root" mysql-root-pass)
-  (template-file "etc/cinder/api-paste.conf" cinder "restart-cinder")
+  (mysql/grant "ALL" "cinder.*" "'cinder'@'%'" "root" mysql-root-pass)
+  (template-file "etc/cinder/api-paste.ini" cinder "restart-cinder")
   (template-file "etc/cinder/cinder.conf" cinder "restart-cinder")
   (exec-script "cinder-manage db sync")
-  (exec-script "dd if=/dev/zero of=cinder-volumes bs=1 count=0 seek=2G")
-  (exec-script "losetup /dev/loop2 cinder-volumes")
-  (exec-script "echo \"n\np\n1\n\n\nt\n8e\nw\n\" | fdisk /dev/loop2")
-  (exec-script "pvcreate /dev/loop2")
-  (exec-script "vgcreate cinder-volumes /dev/loop2")
+  (exec-script
+"if ! fdisk -l /dev/loop2 | grep '/dev/loop2p1'; then
+dd if=/dev/zero of=cinder-volumes bs=1 count=0 seek=2G;
+losetup /dev/loop2 cinder-volumes;
+echo \"n\np\n1\n\n\nt\n8e\nw\n\" | fdisk /dev/loop2 || partprobe;
+pvcreate /dev/loop2;
+vgcreate cinder-volumes /dev/loop2;
+fi;")
   (restart-services :flag "restart-cinder"
                     "cinder-api" "cinder-scheduler" "cinder-volume"))
 
